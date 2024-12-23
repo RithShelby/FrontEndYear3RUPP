279170b526af34d14d9f3933b431bf6c386a6da2
Chhun_Natharith <johnathanrith@gmail.com>
2024-12-23 17:55:15 +0700

    JS

---
+++ Code/JS/index.js
@@ -0,0 +1,20 @@
+// let score = 85;
+// if (score >= 90) {
+//   console.log("Grade A");
+// } else if (score >= 80) {
+//   console.log("Grade B");
+// } else {
+//   console.log("Grade C");
+// }
+// for (let i = 0; i < 5; i++) {
+//   console.log("Interation : ", i);
+// }
+// let arr = ["Red", "Blue", "Green", "Black", "White"];
+
+// for (let index = 0; index < arr.length; index++) {
+//   console.log(arr[index]);
+// }
+// function greet(name) {
+//   return `Hello ${name}`;
+// }
+// console.log(greet("Seek"));
